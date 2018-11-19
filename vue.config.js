b6df6eab6eb4269f0f8616faeea2a921fba3dae5
irie-shinnosuke <irie-shinnosuke@kayac.com>
2018-11-19 11:52:39 +0900

    meta情報とか追加できるようにする

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+const path = require("path");
+const { BASE_PATH, SITE_ORIGIN, META } = require("./src/constants.json");
+
+const TEMPLATE_PARAMS = {
+	SITE_ORIGIN,
+	META,
+	BASE_PATH,
+	BASE_URL: path.join(SITE_ORIGIN, BASE_PATH)
+};
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -5,5 +15,20 @@ module.exports = {
         data: `@import "@/assets/scss/common.scss";`
       }
     }
+  },
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      return args.map(arg => {
+        return Object.assign({}, arg, {
+          templateParameters(params) {
+            return Object.assign(
+              {},
+              arg.templateParameters(params),
+              TEMPLATE_PARAMS
+
+          }
+        });
+      });
+    });
   }
 };
